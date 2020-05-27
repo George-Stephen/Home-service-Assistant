f070e75ae03f51c67598a1af24bf038321a2ecc5
George-Stephen <yazz777stevie@gmail.com>
2020-05-27 10:56:03 +0300

    made a few adjustments to the ui

--- app/src/main/java/com/moringa/homeservice/Login.java
+++ app/src/main/java/com/moringa/homeservice/Login.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -16,12 +17,20 @@ public class Login extends AppCompatActivity {
     @BindView(R.id.emailAddress) EditText LoginEmail;
     @BindView(R.id.loginPassword) EditText LoginPassword;
     @BindView(R.id.loginButton) Button LoginButton;
+    @BindView(R.id.et_signUp) TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/moringa/homeservice/SignUp.java
+++ app/src/main/java/com/moringa/homeservice/SignUp.java
@@ -13,10 +13,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUp extends AppCompatActivity {
-    @BindView(R.id.fullName) EditText FullName;
     @BindView(R.id.Username)EditText username;
     @BindView(R.id.EmailAddress) EditText EmailAddress;
-    @BindView(R.id.Phone)EditText Phone;
     @BindView(R.id.password)EditText Password;
     @BindView(R.id.signUp) Button SignButton;
 
@@ -28,17 +26,13 @@ public class SignUp extends AppCompatActivity {
         SignButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullName = FullName.getText().toString();
                 String Username = username.getText().toString();
                 String Email = EmailAddress.getText().toString();
-                String phone = Phone.getText().toString();
                 String password = Password.getText().toString();
                 Toast.makeText(SignUp.this,"Success",Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(SignUp.this,Search.class);
-                intent.putExtra("fullName",fullName);
                 intent.putExtra("Username",Username);
                 intent.putExtra("Email",Email);
-                intent.putExtra("phone",phone);
                 intent.putExtra("password",password);
                 startActivity(intent);
             }
